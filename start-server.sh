f46067778c01478ec4e25602b3d9b6f51172459e
rahnamafard <arahnamafard@yahoo.com>
2020-11-15 14:43:46 +0330

    start-server.sh

---
+++ start-server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# start-server.sh
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    (cd goldiba/goldiba-back; python manage.py createsuperuser --no-input)
+fi
+(cd goldiba/goldiba-back; gunicorn api.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+nginx -g "daemon off;"
